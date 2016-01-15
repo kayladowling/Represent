386bb746eed7ff77a378fcb7b0f5bef9f3d6660c
delventhalz <delventhalz@gmail.com>
2016-01-14 20:06:00 -0600

    (feat) Cleaned up code.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,20 +3,9 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new Schema({
-
   email: {type: String, required: true, unique: true},
-
   password: {type: String, required: true},
-
   searchCache: [Schema.Types.Mixed]
-
-  // comparePass: function (password, cb) {
-  //   bcrypt.compare(password, this.get('password'), function (err, res) {
-  //     if (err) return console.log(err);
-  //     cb(res);
-  //   });
-  // }
-
 });
 
 userSchema.pre('save', function(next) {
@@ -39,10 +28,6 @@ userSchema.methods.checkPassword = function(password, cb) {
     console.log(result);
     cb(result);
   });
-};
-
-userSchema.methods.validPassword = function(password) {
-  return (this.password === password);
 };
 
 var User = mongoose.model('User', userSchema);
