593d50d80f0692f5e3a577a02d4533743c8319ad
Auggie <jhudak@alumni.berklee.edu>
2016-01-15 14:57:58 -0500

    State Map is fully functioning

--- client/directives/region.js
+++ client/directives/region.js
@@ -1,15 +1,26 @@
-angular.module('Directives').directive('region',['$compile', function($compile){
+angular.module('Directives').directive('region',['$compile', 'SendRequest', '$rootScope', '$state', function($compile, SendRequest, $rootScope, $state){
     return {
       restrict: 'A',
       scope: true,
       link: function(scope,element,attrs){
         scope.elementId = element.attr("id");
-        scope.regionClick = function(){
-          alert(scope.elementId);
-        };
+        scope.regionClick = function() {
+    var url = 'api/byState/'+ scope.elementId;
+    $rootScope.state = scope.elementId;
+    SendRequest.getRequest(url)
+    .success(function(data) {
+      $rootScope.byStateResults = data;
+      $state.go('byStateResults');
+    })
+    .error(function(err) {
+      console.log(err);
+    });
+  };
         element.attr("ng-click", "regionClick()");
         element.removeAttr('region');
         $compile(element)(scope);
       }
     }
   }]);
+
+
