2d74b600cc9466ae8331170fdffbfe97df4246a7
delventhalz <delventhalz@gmail.com>
2016-01-14 19:36:02 -0600

    (feat) Add password hashing on user creation.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,10 +1,36 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new Schema({
+
   email: {type: String, required: true, unique: true},
+
   password: {type: String, required: true},
+
   searchCache: [Schema.Types.Mixed]
+
+  // comparePass: function (password, cb) {
+  //   bcrypt.compare(password, this.get('password'), function (err, res) {
+  //     if (err) return console.log(err);
+  //     cb(res);
+  //   });
+  // }
+
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    console.log(err);
+    console.log(hash);
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
 });
 
 userSchema.methods.validPassword = function(password) {
