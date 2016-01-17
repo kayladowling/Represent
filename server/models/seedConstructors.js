ad4fa4c27b12f81abaf1290226e31275e167d3fb
delventhalz <delventhalz@gmail.com>
2016-01-16 19:43:31 -0600

    (fix) Reworked legacy database seeds to bring in all available information, while maintaining renaming scheme.

--- server/models/seedConstructors.js
+++ server/models/seedConstructors.js
@@ -2,16 +2,10 @@ var senateSeed = require('./senateSeed');
 var houseSeed = require('./houseSeed');
 var CongressPerson = require('./congressPersonModel');
 
-exports.seedSenate = function(){
-  for(var i = 0; i < senateSeed.length; i++){
-    var nextPerson = senateSeed[i];
-    var person = new CongressPerson;
+var seedPerson = function (person, nextPerson) {
     var fullName = nextPerson.first_name + ' ' + nextPerson.last_name;
     person.name = fullName.toLowerCase();
-    person.id = nextPerson.id;
-    person.state = nextPerson.state;
     person.website = nextPerson.url;
-    person.party = nextPerson.party;
     person.nextElection = nextPerson.next_election;
     person.facebook = nextPerson.facebook_account;
     person.twitter = nextPerson.twitter_account;
@@ -19,8 +13,28 @@ exports.seedSenate = function(){
     person.missedVotes = nextPerson.missed_votes;
     person.missedVotesPerc = nextPerson.missed_votes_pct;
     person.votesWithParty = nextPerson.votes_with_party_pct;
+    delete nextPerson.url;
+    delete nextPerson.next_election;
+    delete nextPerson.facebook_account;
+    delete nextPerson.twitter_account;
+    delete nextPerson.total_votes;
+    delete nextPerson.missed_votes;
+    delete nextPerson.missed_votes_pct;
+    delete nextPerson.votes_with_party_pct;
+    for (var key in nextPerson) {
+        person[key] = nextPerson[key];
+    }
+
+    return person;
+};
+
+exports.seedSenate = function(){
+  for(var i = 0; i < senateSeed.length; i++){
+    var nextPerson = senateSeed[i];
+    var person = new CongressPerson;
+    person = seedPerson(person, nextPerson);
+
     person.chamber = 'senate';
-    person.seniority = nextPerson.seniority;
     person.save();
     console.log('senate member');
   }
@@ -30,21 +44,8 @@ exports.seedHouse = function(){
   for(var i = 0; i < houseSeed.length; i++){
     var nextPerson = houseSeed[i];
     var person = new CongressPerson;
-    var fullName = nextPerson.first_name + ' ' + nextPerson.last_name;
-    person.name = fullName.toLowerCase();
-    person.id = nextPerson.id;
-    person.state = nextPerson.state;
-    person.website = nextPerson.url;
-    person.party = nextPerson.party;
-    person.nextElection = nextPerson.next_election;
-    person.facebook = nextPerson.facebook_account;
-    person.twitter = nextPerson.twitter_account;
-    person.totalVotes = nextPerson.total_votes;
-    person.missedVotes = nextPerson.missed_votes;
-    person.missedVotesPerc = nextPerson.missed_votes_pct;
-    person.votesWithParty = nextPerson.votes_with_party_pct;
+    person = seedPerson(person, nextPerson);
     person.chamber = 'house';
-    person.seniority = nextPerson.seniority;
     person.save();
     console.log('house member');
   }
